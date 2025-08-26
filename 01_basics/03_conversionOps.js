f2b4d130490ce268c6822eb08de77038ba8aca55
Md Faran Alam <mdfaranalam11@gmail.com>
2025-08-26 18:41:43 +0000

    conversion

---
+++ 01_basics/03_conversionOps.js
@@ -0,0 +1,25 @@
+//let score = null
+let score = undefined
+
+// console.log(typeof(score));
+
+let valueinNumber = Number(score)
+// console.log(typeof(valueinNumber));
+// console.log(valueinNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedin = "faran"
+let booleanisLoggedin = Boolean(isLoggedin)
+//console.log(booleanisLoggedin)
+
+// 1 -> true; 0 -> false
+// " " -> false;
+// "faran" -> true
+
+let someNum = 66
+let stringNum = String(someNum)
+console.log(stringNum);
+console.log(typeof stringNum);
